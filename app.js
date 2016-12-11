94f8c8ea20dc3cbd237b1d78098aee8337088c7a
ximet <dq.ximet@gmail.com>
2016-12-11 14:34:15 +0300

    add commander for check param from terminal

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const fs = require('fs')
 const fetch = require('node-fetch');
+const commander = require('commander');
+
+commander
+    .version('0.0.1')
+    .option('-e, --email <email>', 'Account email your video system(egghead, youtube, etc...)')
+    .option('-p, --password [password]', 'Account password')
+    .option('-c, --count', 'Add the count number of the video to the filename')
+    .parse(process.argv);
+
+if (process.argv.slice(2).length < 2) {
+    commander.outputHelp();
+    process.exit();
+}
