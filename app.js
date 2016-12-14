20caa0193c0f21534476dba86ae124610ffa2938
ximet <dq.ximet@gmail.com>
2016-12-14 12:06:15 +0300

    update bug auth. delete node-fetch

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
-const fetch = require('node-fetch');
 const commander = require('commander');
+const requestPromise = require('request-promise');
+const fetch = requestPromise.defaults({jar: true});
+
 const SIGN_IN_URL = 'https://egghead.io/users/sign_in';
 commander
     .version('0.0.1')
@@ -18,46 +20,43 @@ downloadVideo();
 
 
 function downloadVideo () {
-    if (commander.email) {
-        authenticate(commander.email, commander.password)
-            .then(res => {
-                console.log('Authenticated!')
-            })
-            .catch(err => console.log('Error:', err))
-    }
-}
-
-function authenticate (email, password) {
-    return getToken()
-        .then(token => {
-            return options = {
-                method: 'POST',
-                form: {
-                    'user[email]': email,
-                    'user[password]': password,
-                    'authenticity_token': token
-                },
-                simple: false,
-                resolveWithFullResponse: true
-            }
+    authenticate(commander.email, commander.password)
+        .then(item => {
+            console.log('Authenticated!')
         })
-        .then(options => fetch(SIGN_IN_URL, options))
-        .then(res => {
-            console.log(res.status);
-            if(res.status !== 302) {
-                throw Error('Failed to authenticate.')
-            }
-        })
+        .catch(err => console.error(err))
 }
 
 
 function getToken () {
     return fetch(SIGN_IN_URL)
-        .then(res => res.text())
-        .then(body => {
-            const pattern = /<meta name="csrf-token" content="(.*)" \/>/;
-            const [, CSRFToken] = pattern.exec(body) || [];
+            .then(body => {
+                const pattern = /<meta name="csrf-token" content="(.*)" \/>/;
+                const [, CSRFToken] = pattern.exec(body) || [];
 
-            return CSRFToken;
+                return CSRFToken;
     });
+}
+
+function authenticate (email, password) {
+    return getToken()
+            .then(token => {
+                return {
+                    method: 'POST',
+                    uri: SIGN_IN_URL,
+                    form: {
+                        'user[email]': email,
+                        'user[password]': password,
+                        'authenticity_token': token
+                    },
+                    simple: false,
+                    resolveWithFullResponse: true
+                };
+            })
+            .then(options => fetch(options))
+            .then(response => {
+                if (response.statusCode !== 302) {
+                    throw Error('Failed to authenticate.');
+                }
+            });
 }
