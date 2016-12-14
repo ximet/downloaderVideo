8167fa629c888b893a97b6fe93f636b46dd961b2
ximet <dq.ximet@gmail.com>
2016-12-14 18:45:07 +0300

    add url and minor update videoDownloader

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ commander
     .option('-e, --email <email>', 'Account email your video system(egghead, youtube, etc...)')
     .option('-p, --password [password]', 'Account password')
     .option('-c, --count', 'Add the count number of the video to the filename')
+    .option('-u, --url [url]', 'Link video for donwload')
     .parse(process.argv);
 
 if (process.argv.slice(2).length < 2) {
@@ -16,16 +17,51 @@ if (process.argv.slice(2).length < 2) {
     process.exit();
 }
 
-downloadVideo();
+downloadVideo(commander.email, commander.password);
 
-
-function downloadVideo () {
-    authenticate(commander.email, commander.password)
+function downloadVideo (email, password) {
+    authenticate(email, password)
         .then(item => {
             console.log('Authenticated!')
         })
-        .catch(err => console.error(err))
+        .catch(err => console.error(err));
+
+    getVideoData(commander.url)
+        .then(item => item)
 }
 
+function getCountVideoInPlaylist (source, listURLs) {
+    const regexp = /<h4 class="title"><a href="(https:\/\/egghead.io\/lessons\/.+?)">/g;
+    let match = null;
+
+    while ((match = regexp.exec(source))) {
+        listURLs.push(match[1])
+    }
+    console.log(`Found ${listURLs.length} ${(listURLs.length) > 1 ? 'lessons' : 'lesson'}`);
+
+    return listURLs;
+}
+
+function downloadLesson () {
+    //TODO add downloader for 1 lesson
+}
+
+function downloadPlaylist (source, lessonURLs) {
+    getCountVideoInPlaylist(source, lessonURLs);
+    //TODO add download full playlist
+}
+
+function getVideoData (urlValue) {
+        const [, isLesson] = /egghead.io\/lessons\/([^\?]*)/.exec(urlValue) || [];
+
+        return fetch(urlValue)
+            .then(source => {
+                if (isLesson) {
+                    downloadLesson()
+                } else {
+                    let lessonURLs = downloadPlaylist(source, []);
+                }
+        });
+}
 
 
