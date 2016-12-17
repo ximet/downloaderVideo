20d0768923eb973c304206ddba4665faa281a6a3
ximet <dq.ximet@gmail.com>
2016-12-17 16:29:03 +0300

    done beta version downloader video, but have some problem with path

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
+const path = require('path');
 const commander = require('commander');
+const request = require('request');
 const requestPromise = require('request-promise');
 const fetch = requestPromise.defaults({jar: true});
 const { authenticate } = require('./services/AuthService.js');
@@ -17,10 +19,11 @@ if (process.argv.slice(2).length < 2) {
     commander.outputHelp();
     process.exit();
 }
+const outputDir = path.resolve(__dirname, 'video');
 
-downloadVideo(commander.email, commander.password);
+workMain(commander.email, commander.password, outputDir);
 
-function downloadVideo (email, password) {
+function workMain (email, password, outputDir) {
     // authenticate(email, password)
     //     .then(item => {
     //         console.log('Authenticated!')
@@ -29,6 +32,50 @@ function downloadVideo (email, password) {
     const isProAccount = false;
     getVideoData(commander.url, isProAccount)
         .then(videos => {
-            console.log(videos)
+            if (!videos.length) {
+                console.log('no video found!')
+            }
+            console.log(`Found ${videos.length} ${(videos.length) > 1 ? 'videos' : 'video'}`)
+
+            createOutputDirectoryIfNeeded(outputDir);
+
+            videos.map(video => {
+                const labelVideo = video.filename;
+                const stream = fs.createWriteStream(labelVideo);
+
+                getStreamVideoUrl(stream, video.url)
+                    .then(result => stream.close());
+            });
+
+            console.log('Done! Thank you!');
         })
+}
+
+function getStreamVideoUrl (stream, url) {
+    return new Promise((resolve, reject) => {
+        request(url)
+            .on('error', () => {
+                error(`download of '${url}' failed!`, false);
+                reject()
+            })
+            .on('end', () => {
+                resolve();
+            })
+            .pipe(stream);
+    })
+}
+
+function createOutputDirectoryIfNeeded (outputDir) {
+    try {
+        const stats = fs.lstatSync(outputDir);
+        if (!stats.isDirectory()) {
+            console.log(`Can't create the output directory '${outputDir}' because a file with the same name exists`)
+        }
+    } catch (e) {
+        try {
+            fs.mkdirSync(outputDir)
+        } catch (err) {
+            console.log(`Creating the output directory '${outputDir}' failed with error '${err}'`)
+        }
+    }
 }
