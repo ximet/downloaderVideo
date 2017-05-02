9b55a5c7ddffdb64523b87fd8da037308c1a0f51
ximet <dq.ximet@gmail.com>
2017-05-02 20:34:49 +0300

    change fetcher method

--- services/FetcherService.js
+++ services/FetcherService.js
@@ -1,10 +1,8 @@
-const requestPromise = require('request-promise');
-const fetch = requestPromise.defaults({jar: true});
+// const requestPromise = require('request-promise');
+// const fetch = requestPromise.defaults({jar: true});
 const request = require('request');
+const REQUEST_AND_CONNECT_TIMEOUT = 30000;
 
-const fetcher = (options) => {
-    return fetch(options);
-};
 
 const streamFetcher = (options, stream, resolve, reject) => {
     return request(options)
@@ -18,6 +16,34 @@ const streamFetcher = (options, stream, resolve, reject) => {
         .pipe(stream);
 };
 
+const fetcher = (url) => {
+    return new Promise((resolve, reject) => {
+        const XHR = new window.XMLHttpRequest();
+
+        XHR.open('GET', url, true);
+
+        XHR.timeout = REQUEST_AND_CONNECT_TIMEOUT;
+
+        XHR.onreadystatechange = function () {
+            const XHR_READY_STATE_DONE = 4;
+
+            if (XHR.readyState === XHR_READY_STATE_DONE) {
+                resolve({ target: { status: XHR.status, data: XHR.response }});
+            }
+        };
+
+        XHR.onload = resolve;
+        XHR.loadend = resolve;
+
+        XHR.onerror = reject;
+        XHR.onabort = reject;
+        XHR.ontimeout = reject;
+
+        XHR.send();
+    });
+};
+
 module.exports = {
     fetcher, streamFetcher
 };
+
